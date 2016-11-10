44e6c05a32e30a64d440d0c9b0fb19e099c079fc
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-11-10 08:43:22 -0800

    more docs

--- R/subprocess.R
+++ R/subprocess.R
@@ -4,7 +4,25 @@ NULL
 
 #' Start a new child process.
 #' 
+#' In Linux, the usual combination of \code{fork()} and \code{exec()}
+#' is used to spawn a new child process. Standard streams are redirected
+#' over regular unnamed \code{pipe}s.
+#' 
+#' In Windows a new process is spawned with \code{CreateProcess()} and
+#' streams are redirected over unnamed pipes obtained with
+#' \code{CreatePipe()}. However, because non-blocking (\emph{overlapped}
+#' in Windows-speech) read/write is not supported for unnamed pipes,
+#' two reader threads are created for each new child process. These
+#' threads never touch memory allocated by R and thus they will not
+#' interfere with R interpreter's memory management (garbage collection).
+#' 
+#' 
 #' @details
+#' \code{command} is always prepended to \code{arguments} so that the
+#' child process can correcty recognize the name of its executable
+#' via its \code{argv} vector. This is done automatically by
+#' \code{spawn_process}.
+#' 
 #' \code{environment} can be passed as a \code{character} vector whose
 #' elements take the form \code{"NAME=VALUE"}, a named \code{character}
 #' vector or a named \code{list}.
@@ -32,6 +50,15 @@ spawn_process <- function (command, arguments = character(), environment = chara
     command <- chartr('/', '\\', command)
   }
   
+  # handle named environment
+  if (!is.null(names(environment))) {
+    if (any(names(environment()) == "")) {
+      stop("empty name(s) for environment variables", call. = FALSE)
+    }
+    environment <- paste(names(environment), as.character(environment), sep = '=')
+  }
+  
+  # hand over to C
   .Call("C_process_spawn", command, c(command, as.character(arguments)),
         as.character(environment), as.character(workdir))
 }
