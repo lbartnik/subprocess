a52101f482bb44b67f1fa27a184c2b697db98ceb
Lukasz Bartnik <l.bartnik@gmail.com>
2016-11-12 06:59:02 -0800

    fix test runner for Windows

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,18 @@
 library(testthat)
 library(subprocess)
 
+# See for more details:
+# https://github.com/hadley/testthat/issues/144
+# https://github.com/hadley/testthat/issues/86
+# https://github.com/luckyrandom/cmaker/commit/b85813ac2b7aef69932eca8fbb4fa0ec225e0af0
+#
+# When R CMD check runs tests, it sets R_TESTS. When the tests
+# themeselves run R CMD xxxx, as is the case with the tests in
+# devtools, having R_TESTS set causes errors because it confuses
+# the R subprocesses. Unsetting it here avoids those problems.
+#
+# We need this because we spawn child R processes and they get very
+# confused if this variable (R_TESTS) is present in their environment.
+Sys.setenv("R_TESTS" = "")
+
 test_check("subprocess")
