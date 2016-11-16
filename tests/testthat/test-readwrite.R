708f3ca3a9fbc49e7442a7ac28d1b4cf81fd2194
Lukasz Bartnik <l.bartnik@gmail.com>
2016-11-16 07:57:13 -0800

    write the command, not pass in args

--- tests/testthat/test-readwrite.R
+++ tests/testthat/test-readwrite.R
@@ -6,12 +6,12 @@ test_that("output buffer is flushed", {
   lines   <- 1000
   command <- paste0('cat(sep = "\\n", replicate(', lines,
                     ', paste(sample(letters, 60, TRUE), collapse = "")))')
-  handle <- spawn_process(R_binary(), c('--slave', '-e', command))
-  
-  # give him a chance to start
-  Sys.sleep(0.3)
+  handle <- R_child()
   expect_true(process_exists(handle))
 
+  # send the command
+  process_write(handle, paste(command, "\n"))
+  
   # read everything
   output <- process_read(handle, 'stdout', TIMEOUT_INFINITE, flush = TRUE)
 
