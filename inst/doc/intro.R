6ccf197fb48307796f00aa25c4f5827db46492a3
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-11-20 22:40:03 -0800

    update other vignette files

--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -2,33 +2,48 @@
 library(subprocess)
 library(knitr)
 
-eval <- .Platform$OS.type != "windows"
-knitr::opts_chunk$set(collapse = TRUE, comment = "#>", eval = eval)
+# if Windows, don't evaluate any code
+eval_ssh <- tolower(.Platform$OS.type != "windows")
 
-## ------------------------------------------------------------------------
+# if not Windows, make sure ssh can connect with the account
+if (eval_ssh) {
+  rc <- system2("ssh", c("test@localhost", "-o", "PasswordAuthentication=no"),
+                stdout = FALSE, stderr = FALSE, input = "")
+  eval_ssh <- (rc == 0)
+}
+
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
+
+## ----include=!eval_ssh, eval=!eval_ssh, echo=FALSE, results='asis'-------
+#  cat("##Important!\n")
+#  cat("This vignette has been evaluated in an environment where the _ssh_\n")
+#  cat("connection to _test&#64;localhost_ cannot be established and thus the\n")
+#  cat("code in the _ssh_ example is not evaluated but simply printed.")
+
+## ----eval=eval_ssh-------------------------------------------------------
 library(subprocess)
 
 ssh_path <- '/usr/bin/ssh'
 handle <- spawn_process(ssh_path, c('-T', 'test@localhost'))
 
-## ------------------------------------------------------------------------
+## ----eval=eval_ssh-------------------------------------------------------
 print(handle)
 
-## ------------------------------------------------------------------------
+## ----eval=eval_ssh-------------------------------------------------------
 process_read(handle, timeout = 1000)
 process_read(handle, 'stderr')
 
-## ------------------------------------------------------------------------
+## ----eval=eval_ssh-------------------------------------------------------
 process_write(handle, 'ls\n')
 process_read(handle, timeout = 1000)
 process_read(handle, 'stderr')
 
-## ------------------------------------------------------------------------
+## ----eval=eval_ssh-------------------------------------------------------
 process_write(handle, 'exit\n')
 process_read(handle, timeout = 1000)
 process_read(handle, 'stderr')
 
-## ------------------------------------------------------------------------
+## ----eval=eval_ssh-------------------------------------------------------
 process_poll(handle)
 process_return_code(handle)
 
